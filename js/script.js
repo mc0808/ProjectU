eb089b1a7d081bbf93d86328394bf4ee8a1938ce
Maksim <mc0808@gmail.com>
2022-04-16 17:42:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//const result = confirm("Ты тут чувак");
+//console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "")
+// console.log(typeof(answer));
+
+const category = "car";
+console.log(`https://mysait.com/${category}`);
+
+const myname = "Maks";
+alert(`Hello ${myname}`);
+
